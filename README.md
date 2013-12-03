144b295d4a4f81a63a81ac3e1f55237277e2b66f
Hugo Giraudel <hugo.giraudel@gmail.com>
2013-12-03 19:16:34 +0100

    Fixing some shits

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # SassyLists
 
-
 This is the future version of SassyLists. Requires Sass 3.3. Check [http://sassylists.com](http://sassylists.com) for stable version.
--- lib/SassyLists.rb
+++ lib/SassyLists.rb
@@ -5,7 +5,7 @@ Compass::Frameworks.register('SassyLists', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyLists
-  VERSION = "0.3.5"
+  VERSION = "1.0.0.alpha"
   DATE = "2013-11-09"
 end
 
