3765c2150b7687cdcb3aec6c6190c508715b1011
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-03-09 14:18:20 +0100

    Bumping 1.0.0

--- lib/SassyLists.rb
+++ lib/SassyLists.rb
@@ -5,8 +5,8 @@ Compass::Frameworks.register('SassyLists', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyLists
-  VERSION = "0.4.9"
-  DATE = "2014-01-20"
+  VERSION = "1.0.0"
+  DATE = "2014-03-09"
 end
 
 module Sass::Script::Functions
