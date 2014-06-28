10f9430e32a1998528ea394c06fdc79996b3a64d
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-28 08:40:36 +0200

    Removed Ruby random function

--- lib/SassyLists.rb
+++ lib/SassyLists.rb
@@ -10,7 +10,5 @@ module SassyLists
 end
 
 module Sass::Script::Functions
-  def random(max = Sass::Script::Number.new(100))
-    Sass::Script::Number.new(rand(max.value), max.numerator_units, max.denominator_units)
-  end
+
 end
