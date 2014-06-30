eea1da3566b433eb0028cf0a3e94ddfba070b009
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-30 09:09:30 +0200

    Bumped gem version

--- lib/SassyLists.rb
+++ lib/SassyLists.rb
@@ -5,7 +5,7 @@ Compass::Frameworks.register('SassyLists', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module SassyLists
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
   DATE = "2014-06-28"
 end
 
